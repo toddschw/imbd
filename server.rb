42e5e9b3a48764cfd07c256dcb9d6bb3ad48d22b
Damon Davison <damon@curatur.com>
2015-10-28 14:25:43 -0400

    Look up films via url (params)

--- server.rb
+++ server.rb
@@ -5,7 +5,7 @@ get "/" do
   erb :index
 end
 
-get "/movie" do
-  movie = Siskel.new("Lion King")
-  "The title of our movie is #{movie.title}."
+get "/movie/:name" do
+  @movie = Siskel.new(params["name"])
+  erb :movie
 end
