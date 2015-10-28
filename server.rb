0204055a4fe2a2ce19b441703aac7f9b6d00db36
Damon Davison <damon@curatur.com>
2015-10-28 14:05:16 -0400

    Use Siskel to fetch movie

--- server.rb
+++ server.rb
@@ -1,5 +1,11 @@
 require "sinatra"
+require_relative "models/siskel"
 
 get "/" do
   erb :index
+end
+
+get "/movie" do
+  movie = Siskel.new("Lion King")
+  "The title of our movie is #{movie.title}."
 end
