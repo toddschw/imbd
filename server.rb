6ffe4ec6e1dc52f0952aa2c8e9c19c704e7b3b5e
Todd Schwartzfarb <todd@wirestreak.com>
2015-12-10 21:27:21 -0500

    modified index page

--- server.rb
+++ server.rb
@@ -2,13 +2,10 @@ require "sinatra"
 require_relative "models/siskel"
 
 get "/" do
-  erb :index
-end
-
-get "/movie/:name" do
-  @flick = Siskel.new(params["name"])
+  @flick = Siskel.new("superman")
   erb :movie
 end
+
 
 get "/movie?" do
    searched_movie = params['title']
